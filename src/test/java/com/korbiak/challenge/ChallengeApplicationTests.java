2b51e750bfb0b49066aa9af96c6b09e777dd637e
Maksym Korbiak <Maksym_Korbiak@epam.com>
2020-08-21 15:57:25 +0300

    creation project

---
+++ src/test/java/com/korbiak/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korbiak.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
