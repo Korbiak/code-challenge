5e586c6a3205c4e48cf6b85d3b0765c201c8b625
Maksym Korbiak <Maksym_Korbiak@epam.com>
2020-08-21 16:18:41 +0300

    creation h2 db

---
+++ src/main/resources/static/schema.sql
@@ -0,0 +1,39 @@
+CREATE TABLE Category
+(
+    id   INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(32) NOT NULL UNIQUE
+);
+
+CREATE TABLE Product
+(
+    id     INT AUTO_INCREMENT PRIMARY KEY,
+    name   VARCHAR(32) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    price DOUBLE NOT NULL,
+    release_date DATE NOT NULL,
+    category_id INT NOT NULL,
+
+    CONSTRAINT fk_category
+        FOREIGN KEY (category_id)
+            REFERENCES Category (id)
+);
+
+CREATE TABLE User
+(
+    id   INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(32) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    admin_permissions BOOLEAN NOT NULL
+);
+
+INSERT INTO Category(name)
+VALUES ('footwear'), ('clothes');
+
+INSERT INTO Product(name, description, price, release_date, category_id)
+VALUES
+('Core Red', 'Example of text', 100.2, parsedatetime('17-09-2018', 'dd-MM-yyyy'), 1),
+('TUC', 'Example of text', 69.3, parsedatetime('6-11-2019', 'dd-MM-yyyy'), 2);
+
+INSERT INTO User(name, password, admin_permissions)
+VALUES ('admin', '$2a$10$sOl0xuyeqvtH/5SZTXwLLeJtB/nMulcJKoLPuoKqFevCgUTOIK9SC', true );
+-- password = 'admin' for this user
